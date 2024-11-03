9990d93c1476d141a977c3be254cd2048e0b7f38
Claes Erik <claeserikjr@gmail.com>
2024-11-03 02:01:45 -0300

    initial commit

---
+++ src/main/java/com/example/PlaylistDev/PlaylistDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.PlaylistDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PlaylistDev/PlaylistDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PlaylistDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
